c3ff0ae48a8af4e41fbf85b9b30907dd94e64b99
Biswajit <biswajitmajumderjee@gmail.com>
2023-07-09 16:55:54 +0530

    footer-changing

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Image from 'next/image'
-import HeroSection from '@/components/comps/landing-page-comps/herosection'
+import Footer from "@/components/comps/landing-page-comps/footer"
+// import HeroSection from '@/components/comps/landing-page-comps/herosection'
+// import HeroSection from '@/components/comps/landing-page-comps/herosection'
+// import HeroSection from '@/components/comps/landing-page-comps/herosection'
+// import HeroSection from '@/components/comps/landing-page-comps/herosection'
+import Branding from "@/components/comps/landing-page-comps/branding"
 
 export default function Home() {
   return (
-    <main className="w-full">
-      {/* <h1 className="text-4xl font-bold text-center">Hello world</h1> */}
-      {/* <Image src="/images/nextjs.svg" width={300} height={300} alt='something about something' /> */}
-      <HeroSection></HeroSection>
+    <main className="">
+        <Footer/>
+        <Branding/>
     </main>
   )
 }
