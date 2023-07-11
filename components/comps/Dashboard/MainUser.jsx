d9305fadb29ffd140691aed4e86c170be68eca9f
SouZe-San <2003soumyajit@gmial.com>
2023-07-11 09:57:53 +0530

    Dashboard Style Working

--- components/comps/Dashboard/MainUser.jsx
+++ components/comps/Dashboard/MainUser.jsx
@@ -5,8 +5,9 @@ import "../../styles/Dashboard/mainUser.scss";
 
 const MainUser = () => {
   return (
-    <div className="w-full flex justify-center">
-      <div className="max-w-sm rounded overflow-hidden shadow-lg justify-self-center  user_card">
+    <div className="w-full flex justify-center flex-col mainUser_div">
+      {/* DEFECTED STYLE */}
+      {/* <div className="max-w-sm rounded overflow-hidden shadow-lg justify-self-center  user_card">
         <div className=" text-center mb-4">
           <h1 className="user_avatar">HK</h1>
         </div>
@@ -25,6 +26,41 @@ const MainUser = () => {
             <p className="text-white text-[1.2rem;]">
               <span className="font-bold"> Team Members:</span> 3
             </p>
+          </div>
+        </div>
+      </div> */}
+
+      {/* -------- NEW STYLE ----------- */}
+      <div className="hunger"></div>
+      <div className="card_upper_part">
+        <div className="card_inner_part">
+          <div className="bg w-full bg-gradient-to-r from-green-500 to-blue-500  h-[27%]"></div>
+          <svg xmlns="http://www.w3.org/2000/svg" width="170" height="150.21" viewBox="0 0 170 150">
+            <polygon
+              points="100,20 150,50 150,110 100,140 50,110 50,50"
+              fill="none"
+              stroke-width="15"
+            />
+          </svg>
+
+          <div className="user_details mb-4">
+            <div className="score">445</div>
+            <h2>Elf Kingdom</h2>
+
+            <div className="box flex">
+              <div className="flag_num">
+                34
+                <p>Flag</p>
+              </div>
+              <div className="members">
+                4<p>Members</p>
+              </div>
+
+              <div className="completion">
+                84%
+                <p>Done</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- components/comps/Dashboard/RankingBlock.jsx
+++ components/comps/Dashboard/RankingBlock.jsx
@@ -1,30 +1,49 @@
 import React from "react";
-
+import Image from "next/image";
 // Components
 import Rankers from "./Rankers";
 import MainUser from "./MainUser";
+
+import "../../styles/Dashboard/dashboard.scss";
 
 // Data
 import { Users } from "./user";
 
 const RankingBlock = () => {
   return (
-    <div className="px-32">
-      <h1 className="w-full text-4xl font-bold text-center text-green-400  mb-4 px-32">
-        This Is The Main RANKING BOARD
+    <div className="px-32 flex flex-col items-center">
+      {/* <Image
+        src="/strip.png"
+        alt="this Is Strip"
+        height={500}
+        width={500}
+        style={{
+          top: 0,
+          zIndex: -1,
+          aspectRatio: 1,
+          minWidth: "50%",
+        }}
+        className="h-[20rem] absolute"
+      /> */}
+
+      <h1 className="w-full text-[3rem] font-bold text-center text-green-400  mb-4 px-32 md:pt-8 md:pb-12">
+        The Main RANKING BOARD
       </h1>
-      {/* Details Of Login Team */}
+      <h1 className="bg_text">
+        C{"  "}T{"  "}F{"\n"} CHAMPIONSHIP
+      </h1>
+      {/* -------- Details Of Login Team */}
       <MainUser />
 
-      {/* Ranking Table */}
+      {/* -------- Ranking Table */}
 
-      {Users.sort((curr, next) => curr.rank - next.rank).map((user) => {
+      {/* {Users.sort((curr, next) => curr.rank - next.rank).map((user) => {
         return (
           <div key={user.rank}>
             <Rankers user={user} />
           </div>
         );
-      })}
+      })} */}
     </div>
   );
 };
