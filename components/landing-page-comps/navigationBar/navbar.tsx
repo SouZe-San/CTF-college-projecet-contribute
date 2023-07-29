e8d0232269b59e710626b23149ff3a757b00f62f
SouZe-San <2003soumyajit@gmial.com>
2023-07-29 10:05:11 +0530

    lunch-after-nav

--- components/landing-page-comps/navigationBar/navbar.tsx
+++ components/landing-page-comps/navigationBar/navbar.tsx
@@ -2,35 +2,35 @@
 "use client";
 import "./navbar.scss";
 import Link from "next/link";
-import homeSvg from "@/public/nav-section-svgs/home-line.svg";
-import profileSvg from "@/public/nav-section-svgs/user-line.svg";
-import { useEffect, useState } from "react";
+// import homeSvg from "@/public/nav-section-svgs/home-line.svg";
+// import profileSvg from "@/public/nav-section-svgs/user-line.svg";
+// import { useEffect, useState } from "react";
 
-function Hello(){
-  return (
-    <div className="mobile-ones ">
-            <div className="menu flex justify-between">
-              <ul>
-                <li>
-                  <Link href="/">{homeSvg}</Link>
-                </li>
-                <li>
-                  {/* <Link href="/#RewardsSection">Rewards</Link> */}
-                </li>
-                <li>
-                  {/* <Link href="/#TopicsSection">Topics</Link> */}
-                </li>
-                <li>
-                  {/* <Link href="/#RulesSection">Rules</Link> */}
-                </li>
-                <li>
-                  <Link href="/dashboard">{profileSvg}</Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-  )
-}
+// function Hello(){
+//   return (
+//     <div className="mobile-ones ">
+//             <div className="menu flex justify-between">
+//               <ul>
+//                 <li>
+//                   <Link href="/">{homeSvg}</Link>
+//                 </li>
+//                 <li>
+//                   {/* <Link href="/#RewardsSection">Rewards</Link> */}
+//                 </li>
+//                 <li>
+//                   {/* <Link href="/#TopicsSection">Topics</Link> */}
+//                 </li>
+//                 <li>
+//                   {/* <Link href="/#RulesSection">Rules</Link> */}
+//                 </li>
+//                 <li>
+//                   <Link href="/dashboard">{profileSvg}</Link>
+//                 </li>
+//               </ul>
+//             </div>
+//           </div>
+//   )
+// }
 
 function NavBar() {
  
